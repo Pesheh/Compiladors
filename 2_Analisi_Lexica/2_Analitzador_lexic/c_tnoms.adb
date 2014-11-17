8749e075a0be6f5d5bcf404c75552667f392af61
PereBal <PereBal@github.com>
2014-11-17 22:20:12 +0100

    Changes to be committed: 	modified:   2_Analisi_Lexica/2_Analitzador_lexic/c_tnoms.adb 	modified:   2_Analisi_Lexica/2_Analitzador_lexic/clean.sh 	modified:   2_Analisi_Lexica/2_Analitzador_lexic/pershe.l 	modified:   6_Executables/al-file2

--- 2_Analisi_Lexica/2_Analitzador_lexic/c_tnoms.adb
+++ 2_Analisi_Lexica/2_Analitzador_lexic/c_tnoms.adb
@@ -1,51 +1,50 @@
-with d_token,a_lexic,Ada.Integer_Text_IO,Ada.Strings.Unbounded,Ada.Text_IO, Ada.Command_Line; use d_token,a_lexic,Ada.Strings.Unbounded,Ada.Text_IO, Ada.Command_Line,Ada.Integer_Text_IO;
-with d_tnoms, general_defs; use d_tnoms, general_defs;
-procedure C_Tnoms is
-	tk		: Token;
-	s		: Unbounded_String;
-	tn		: tnoms;
-	i,i2,j		: Integer;
-	id		: general_defs.id_nom;
-	ids		: array(1..50) of general_defs.id_nom; --50 son prous elements per un joc de proves petit
-	id_st 	: general_defs.id_str;
-	ids_str	: array(1..50) of general_defs.id_str;
-begin 
-	if Argument_Count=1 then
-		open(Argument(1)); --per fer proves
-	else
-		open("./al-file");
-	end if;
-	i:=1; i2:=1;
-	empty(tn);
-	Read_Input:
-	loop 
-		tk:=YYLex;
-		s:=To_Unbounded_String(YYText);
+
+	with Ada.Text_IO,Ada.Integer_Text_IO,Ada.Command_Line; use Ada.Text_IO,Ada.Integer_Text_IO,Ada.Command_Line;
+	with d_token,a_lexic; use d_token,a_lexic;
+	with d_tnoms, general_defs; use d_tnoms, general_defs;
+	procedure C_Tnoms is
+		tk		: Token;
+		tn		: tnoms;
+		i,i2,j		: Integer;
+		id		: general_defs.id_nom;
+		ids		: array(1..50) of general_defs.id_nom; --50 son prous elements per un joc de proves petit
+		id_st 	: general_defs.id_str;
+		ids_str	: array(1..50) of general_defs.id_str;
+	begin 
+		if Argument_Count=1 then
+			open(Argument(1)); --per fer proves
+			i:=1; i2:=1;
+			empty(tn);
+			Read_Input:
+			loop 
+				tk:=YYLex;
 		
-		case tk is
-			when Error => Put_Line("Error: "&To_String(s));
-			when Identifier => 	d_tnoms.put(tn,To_String(s),id);ids(i):=id; i:=i+1; Put_Line("ID:"&To_String(s)); 
-			when Str => d_tnoms.put(tn,To_String(s),id_st); ids_str(i2):=id_st; i2:=i2+1; Put_Line("STR:"&To_String(s));
-			when End_Of_Input => Put_Line("EOF");
-			when others => Put_Line(To_String(s));
-		end case;
+				case tk is
+					when Identifier => 	d_tnoms.put(tn,YYText,id); New_Line(1);ids(i):=id; i:=i+1; Put_Line("ID:"&YYText); 
+					when Str => d_tnoms.put(tn,YYText,id_st); New_Line(1);ids_str(i2):=id_st; i2:=i2+1; Put_Line("STR:"&YYText);
+					when End_Of_Input => Put_Line("EOF");
+					when others => New_Line(1);
+				end case;
 
+			exit Read_Input
+				when tk= End_of_Input;
+			end loop Read_Input;
 
-		exit Read_Input
-			when tk= End_of_Input;
-	end loop Read_Input;
+			Put_Line("Tnoms content:");
+			j:=1; 
+			while j<i loop
+				Put_Line("Id("&ids(j)'Img&"):"&get(tn,ids(j)));
+				j:=j+1;
+			end loop;
+			j:=1;
+			while j<i2 loop
+				Put_Line("Str("&ids_str(j)'Img&"):"&get(tn,ids_str(j)));
+				j:=j+1;
+			end loop;
+			close;
+		else
+			Put("Usage: ./c_tnoms <file name>.p");
+		end if;
+	end C_Tnoms;
 
-	Put_Line("Tnoms content:");
-	j:=1; 
-	while j<i loop
-		Put_Line("Id("&ids(j)'Img&"):"&get(tn,ids(j)));
-		j:=j+1;
-	end loop;
-	j:=1;
-	while j<i2 loop
-		Put_Line("Str("&ids_str(j)'Img&"):"&get(tn,ids_str(j)));
-		j:=j+1;
-	end loop;
-	close;
-end C_Tnoms;
 
--- 2_Analisi_Lexica/2_Analitzador_lexic/clean.sh
+++ 2_Analisi_Lexica/2_Analitzador_lexic/clean.sh
@@ -5,5 +5,6 @@ rm -f *.a
 rm -f *.o
 rm -f *.ali
 rm -f a_lex*
+rm -f c_tnoms.adb
 rm -f example
 rm -f c_tnoms
